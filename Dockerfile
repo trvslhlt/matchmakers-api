7e079b6b8c034df3c057b5fdd1adc4c47598427a
travis holt <trvslhlt@gmail.com>
2021-01-22 00:24:34 -0500

    serve static files

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,11 @@
 FROM node:15.6.0-alpine3.10
+
+RUN mkdir /project
+
+COPY package.json /project
+
+WORKDIR /project
+
+RUN npm install
+
+COPY . /project
---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log('listening');
+});
